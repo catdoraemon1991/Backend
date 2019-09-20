b93028a9bc530c5f2c79e7411318b3423bb545f6
Zui Chen <zuic@umich.edu>
2019-09-20 02:26:05 -0400

    header, logo, icon updates

--- app/src/main/java/com/laioffer/botlogistics/ControlPanel.java
+++ app/src/main/java/com/laioffer/botlogistics/ControlPanel.java
@@ -16,6 +16,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.arlib.floatingsearchview.FloatingSearchView;
 import com.arlib.floatingsearchview.suggestions.model.SearchSuggestion;
 import com.google.android.material.navigation.NavigationView;
